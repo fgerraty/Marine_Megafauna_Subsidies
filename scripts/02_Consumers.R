8a6d7b496f2c8b68672d9407994cb4310389d6ee
Frankie Gerraty <frankiegerraty@gmail.com>
2024-08-09 12:40:59 -0700

    Pre color change

--- scripts/02_Consumers.R
+++ scripts/02_Consumers.R
@@ -9,34 +9,7 @@
 consumers <- read_csv("data/processed/consumers.csv")
 filtered_consumers <- read_csv("data/processed/filtered_consumers.csv")
 
-# Part 2: Diversity Sankey Plot with ggsankey ----------------------------
-
-
-plot_df <- filtered_consumers %>% 
-  make_long(marine_megafauna_group, consumer_class) %>% 
-  mutate(node = factor(node, levels = c(
-    "Armadillo", "Bat", "Sea Turtles", "Bird", 
-    "Sirenian", "Fissipeds","Pinnipeds", "Carnivore", 
-    "Rodent", "Marsupial", "Cetaceans", "Ungulate",  "Reptile")))
-
-ggplot(plot_df, aes(x = x, 
-                    next_x = next_x, 
-                    node = node, 
-                    next_node = next_node,
-                    fill = factor(node), 
-                    label = node)) +
-  geom_sankey() +
-  scale_fill_viridis_d(alpha = 0.85, drop = FALSE) +
-  theme_sankey(base_size = 10) +
-  theme(legend.position = "none") +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())#+
- #geom_sankey_label() #toggles whether ot not there are labels on each node
-
-
-
-# Part 2: Diversity Sankey Plot with ggalluvial --------------------------------
+# Part 2: Diversity sankey plot with ggalluvial --------------------------------
 
 #Create color palette: 
 
@@ -52,7 +25,7 @@ stratum_pal <- c("transparent", "#443983", "transparent", "#31688e",
 flow_pal <- c("blank1" = "transparent", "blank2"="transparent", "blank3"= "transparent", 
               "blank4"= "transparent", "blankA" = "transparent","blankB" = "transparent",
               "Cetaceans" =  "#90d743", "Pinnipeds" = "#20a486", 
-              "Sirenian" = "#287c8e", "Fissiped" = "#31688e", 
+              "Fissipeds" = "#287c8e", "Sirenian" = "#31688e", 
               "Sea Turtles" = "#443983")
                  
 
@@ -84,11 +57,140 @@ plot_df <- filtered_consumers %>%
 ggplot(data=plot_df, aes(axis1=marine_megafauna_group, 
                                     axis2=consumer_class,
                                     y=freq))+
-  geom_alluvium(width = 1/12, curve_type = "sigmoid", aes(fill = marine_megafauna_group))+
+  geom_alluvium(width = 1/12, curve_type = "sigmoid", alpha = .75,
+                aes(fill = marine_megafauna_group))+
   scale_fill_manual(values = flow_pal, guide = "none") +
   geom_stratum(width=1/12, fill = stratum_pal, color = "transparent")+
 #  geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off
   theme_void()
   
+ggsave("output/extra_plots/sankey_figure/diversity_sankey.png", width = )
   
+
+# Part 3: Interaction type sankey plots with ggalluvial --------------------------------
+
+#Part 3A: Scavenging sankey plot ####
+
+scav_sankey_df <- consumers %>% 
+  group_by(marine_megafauna_group, consumer_class, scavenging) %>% 
+  summarise(freq = n(), .groups="drop") %>% 
+  bind_rows(blank_rows) %>% 
+  mutate(marine_megafauna_group = factor(marine_megafauna_group, 
+                                         levels = c("blankA","Cetaceans","blank1","Pinnipeds",
+                                                    "blank2", "Fissipeds","blank3",
+                                                    "Sirenian","blank4","Sea Turtles", "blankB")),
+         consumer_class = factor(consumer_class, levels = c("blankC", "Ungulate", "blank5",
+                                                            "Rodent","blank6","Reptile",
+                                                            "blank7","Marsupial", "blank8", 
+                                                            "Carnivore", "blank9", "Bird",
+                                                            "blank10","Bat","blank11", 
+                                                            "Armadillo")))
+
+
+ggplot(data=scav_sankey_df, aes(axis1=marine_megafauna_group, 
+                         axis2=consumer_class,
+                         y=freq))+
+  geom_alluvium(width = 1/12, curve_type = "sigmoid", alpha = .75,
+                aes(fill = scavenging))+
+  scale_fill_manual(values = c("grey80", "#003049"), #FALSE = "grey", TRUE = "orange
+                    na.value = "transparent", #Fill NA values with transparent fill
+                    guide="none") + #remove legend
+  geom_stratum(width=1/12, fill = stratum_pal, color = "transparent")+
+ #   geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off
+  theme_void()
+
+#Part 3B: Predation sankey plot ####
+
+predation_sankey_df <- consumers %>% 
+  group_by(marine_megafauna_group, consumer_class, predation) %>% 
+  summarise(freq = n(), .groups="drop") %>% 
+  bind_rows(blank_rows) %>% 
+  mutate(marine_megafauna_group = factor(marine_megafauna_group, 
+                                         levels = c("blankA","Cetaceans","blank1","Pinnipeds",
+                                                    "blank2", "Fissipeds","blank3",
+                                                    "Sirenian","blank4","Sea Turtles", "blankB")),
+         consumer_class = factor(consumer_class, levels = c("blankC", "Ungulate", "blank5",
+                                                            "Rodent","blank6","Reptile",
+                                                            "blank7","Marsupial", "blank8", 
+                                                            "Carnivore", "blank9", "Bird",
+                                                            "blank10","Bat","blank11", 
+                                                            "Armadillo")))
+
+
+ggplot(data=predation_sankey_df, aes(axis1=marine_megafauna_group, 
+                                axis2=consumer_class,
+                                y=freq))+
+  geom_alluvium(width = 1/12, curve_type = "sigmoid", alpha = .75,
+                aes(fill = predation))+
+  scale_fill_manual(values = c("grey80", "#D62828"), #FALSE = "grey", TRUE = "orange
+                    na.value = "transparent", #Fill NA values with transparent fill
+                    guide="none") + #remove legend
+  geom_stratum(width=1/12, fill = stratum_pal, color = "transparent")+
+  #   geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off
+  theme_void()
+
+#Part 3C: Egg consumption sankey plot ####
+
+egg_sankey_df <- consumers %>% 
+  group_by(marine_megafauna_group, consumer_class, consuming_eggs) %>% 
+  summarise(freq = n(), .groups="drop") %>% 
+  bind_rows(blank_rows) %>% 
+  mutate(marine_megafauna_group = factor(marine_megafauna_group, 
+                                         levels = c("blankA","Cetaceans","blank1","Pinnipeds",
+                                                    "blank2", "Fissipeds","blank3",
+                                                    "Sirenian","blank4","Sea Turtles", "blankB")),
+         consumer_class = factor(consumer_class, levels = c("blankC", "Ungulate", "blank5",
+                                                            "Rodent","blank6","Reptile",
+                                                            "blank7","Marsupial", "blank8", 
+                                                            "Carnivore", "blank9", "Bird",
+                                                            "blank10","Bat","blank11", 
+                                                            "Armadillo")))
+
+
+ggplot(data=egg_sankey_df, aes(axis1=marine_megafauna_group, 
+                                     axis2=consumer_class,
+                                     y=freq))+
+  geom_alluvium(width = 1/12, curve_type = "sigmoid", alpha = .75,
+                aes(fill = consuming_eggs))+
+  scale_fill_manual(values = c("grey80", "#F77F00"), #FALSE = "grey", TRUE = "orange
+                    na.value = "transparent", #Fill NA values with transparent fill
+                    guide="none") + #remove legend
+  geom_stratum(width=1/12, fill = stratum_pal, color = "transparent")+
+  #   geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off
+  theme_void()
+
+
+#Part 3D: Placenta/excreta consumption sankey plot ####
+
+placenta_excreta_sankey_df <- consumers %>% 
+  mutate(consuming_placenta_excreta = if_else(consuming_placenta == TRUE | 
+                                              consuming_excreta == TRUE, 
+                                              TRUE, FALSE)) %>% 
+  group_by(marine_megafauna_group, consumer_class, consuming_placenta_excreta) %>% 
+  summarise(freq = n(), .groups="drop") %>% 
+  bind_rows(blank_rows) %>% 
+  mutate(marine_megafauna_group = factor(marine_megafauna_group, 
+                                         levels = c("blankA","Cetaceans","blank1","Pinnipeds",
+                                                    "blank2", "Fissipeds","blank3",
+                                                    "Sirenian","blank4","Sea Turtles", "blankB")),
+         consumer_class = factor(consumer_class, levels = c("blankC", "Ungulate", "blank5",
+                                                            "Rodent","blank6","Reptile",
+                                                            "blank7","Marsupial", "blank8", 
+                                                            "Carnivore", "blank9", "Bird",
+                                                            "blank10","Bat","blank11", 
+                                                            "Armadillo")))
+
+
+ggplot(data=placenta_excreta_sankey_df, aes(axis1=marine_megafauna_group, 
+                               axis2=consumer_class,
+                               y=freq))+
+  geom_alluvium(width = 1/12, curve_type = "sigmoid", alpha = .75,
+                aes(fill = consuming_placenta_excreta))+
+  scale_fill_manual(values = c("grey80", "#FCBF49"), #FALSE = "grey", TRUE = "orange
+                    na.value = "transparent", #Fill NA values with transparent fill
+                    guide="none") + #remove legend
+  geom_stratum(width=1/12, fill = stratum_pal, color = "transparent")+
+  #   geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off
+  theme_void()
+
 
