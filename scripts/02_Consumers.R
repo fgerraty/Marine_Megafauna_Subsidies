d77d718ecb4e9b044790ba5c624677c6394cba25
Frankie Gerraty <frankiegerraty@gmail.com>
2024-08-09 14:23:26 -0700

    Adding annotations to 02_consumers

--- scripts/02_Consumers.R
+++ scripts/02_Consumers.R
@@ -9,7 +9,7 @@
 consumers <- read_csv("data/processed/consumers.csv")
 filtered_consumers <- read_csv("data/processed/filtered_consumers.csv")
 
-# Part 2: Diversity sankey plot with ggalluvial --------------------------------
+# PART 2: Diversity sankey plot with ggalluvial --------------------------------
 
 #Create color palettes: 
 
@@ -45,11 +45,12 @@ blank_rows <- tibble(
 )
 
 
-
+#Create dataframe for plotting diversity sankey plot
 plot_df <- filtered_consumers %>% 
-  group_by(marine_megafauna_group, consumer_class) %>% 
-  summarise(freq = n(), .groups="drop") %>% 
-  bind_rows(blank_rows) %>% 
+  group_by(marine_megafauna_group, consumer_class) %>% #group by consumer-resource combo
+  summarise(freq = n(), .groups="drop") %>% #count # of spp. combinations in each group
+  bind_rows(blank_rows) %>% #bring in blank rows for plotting aesthetics
+  #Turn axis variables into factors
   mutate(marine_megafauna_group = factor(marine_megafauna_group, 
                                          levels = c("blankA","Cetaceans","blank1","Pinnipeds",
                                                     "blank2", "Fissipeds","blank3",
@@ -61,23 +62,25 @@ plot_df <- filtered_consumers %>%
                                                             "blank10","Bat","blank11", 
                                                             "Armadillo")))
 
-
+#Plot diversity sankey plot
 ggplot(data=plot_df, aes(axis1=marine_megafauna_group, 
                                     axis2=consumer_class,
                                     y=freq))+
+  #"Flows" with geom_alluvium
   geom_alluvium(width = 1/12, curve_type = "sigmoid", alpha = .75,
                 aes(fill = marine_megafauna_group))+
   scale_fill_manual(values = flow_pal, guide = "none") +
+  #axes with geom_stratum
   geom_stratum(width=1/12, fill = stratum_pal, color = "transparent")+
-#  geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off
+#  geom_text(stat = "stratum", aes(label = after_stat(stratum))) + #Toggles labels on/off, remove in final version of script
   theme_void()
   
 ggsave("output/extra_plots/sankey_figure/diversity_sankey.png", width = 4, height = 6, units = "in")
   
 
-# Part 3: Interaction type sankey plots with ggalluvial --------------------------------
+# PART 3: Interaction type sankey plots with ggalluvial --------------------------------
 
-#Part 3A: Scavenging sankey plot ####
+# Part 3A: Scavenging sankey plot ####
 
 scav_sankey_df <- consumers %>% 
   group_by(marine_megafauna_group, consumer_class, scavenging) %>% 
@@ -112,7 +115,7 @@ ggsave("output/extra_plots/sankey_figure/scavenging_sankey.png",
        width = 2, height = 2.5, units = "in")
 
 
-#Part 3B: Predation sankey plot ####
+# Part 3B: Predation sankey plot ####
 
 predation_sankey_df <- consumers %>% 
   group_by(marine_megafauna_group, consumer_class, predation) %>% 
@@ -147,7 +150,7 @@ ggsave("output/extra_plots/sankey_figure/predation_sankey.png",
        width = 2, height = 2.5, units = "in")
 
 
-#Part 3C: Egg consumption sankey plot ####
+# Part 3C: Egg consumption sankey plot ####
 
 egg_sankey_df <- consumers %>% 
   group_by(marine_megafauna_group, consumer_class, consuming_eggs) %>% 
@@ -183,7 +186,7 @@ ggsave("output/extra_plots/sankey_figure/egg_consumption_sankey.png",
 
 
 
-#Part 3D: Placenta/excreta consumption sankey plot ####
+# Part 3D: Placenta/excreta consumption sankey plot ####
 
 placenta_excreta_sankey_df <- consumers %>% 
   mutate(consuming_placenta_excreta = if_else(consuming_placenta == TRUE | 
@@ -221,9 +224,9 @@ ggsave("output/extra_plots/sankey_figure/placenta_excreta_sankey.png",
        width = 2, height = 2.5, units = "in")
 
 
-# Part 3E: Manually create color legend ---------
+# Part 3E: Manually create color legend for interaction type plots ---------
 
-# Define the custom data
+# Define custom data
 legend_data <- data.frame(
   category = c("Scavenging", "Predation", "Egg Consumption", "Placenta and/or Excreta Consumption"),
   x = c(1, 1, 1, 1),
@@ -231,22 +234,25 @@ legend_data <- data.frame(
 ) %>% 
   mutate(category = factor(category, levels = c("Scavenging", "Predation", "Egg Consumption", "Placenta and/or Excreta Consumption")))
 
-# Create the legend plot with square boxes
+# Create plot to extract legend from
 temp_plot <- ggplot(legend_data, aes(x = x, y = y, fill = category)) +
   geom_tile(aes(width = 0.8, height = 0.8)) +
   scale_fill_manual(values = c(
     "Scavenging" = "#003049",
     "Predation" = "#D62828",
     "Egg Consumption" = "#F77F00",
-    "Placenta and/or Excreta Consumption" = "#219ebc"), labels = c("Scavenging", "Predation", "Egg Consumption", "Placenta and/or\nExcreta Consumption")) +
+    "Placenta and/or Excreta Consumption" = "#219ebc"), 
+    labels = c("Scavenging", "Predation", 
+               "Egg Consumption", "Placenta and/or\nExcreta Consumption")) +
   labs(fill = "") +  
   theme_void() +
   theme(legend.position = "bottom")+
-  guides(fill=guide_legend(nrow=2,byrow=TRUE))
-temp_plot
+  guides(fill=guide_legend(nrow=2,byrow=TRUE)) #Make legend into two rows
 
+#extract legend using ggpubr::get_legend
 legend <- get_legend(temp_plot)
-as_ggplot(legend)
+as_ggplot(legend) #turn back into ggplot
 
+#Export legend
 ggsave("output/extra_plots/sankey_figure/legend.png", width = 7, height = 1, units = "in")
 
